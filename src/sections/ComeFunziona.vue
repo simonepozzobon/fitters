10812cfc53bf69dd9d7b4e1c759200b197647dae
Esse <info@simonepozzobon.com>
2019-11-17 05:22:00 +0100

    Come funziona

--- src/sections/ComeFunziona.vue
+++ src/sections/ComeFunziona.vue
@@ -1,15 +1,25 @@
 <template>
-<div class="py-8 px-12">
+<div class="pt-8 pb-24 px-12">
     <ui-title title="Come funziona" />
+    <img
+        :src="comeFunziona"
+        alt=""
+    >
 </div>
 </template>
 
 <script>
 import UiTitle from '../components/UiTitle.vue'
+const comeFunziona = require('../assets/images/come_funziona.svg')
 export default {
     name: 'ChiSiamo',
     components: {
         UiTitle,
+    },
+    data: function () {
+        return {
+            comeFunziona: comeFunziona
+        }
     },
 }
 </script>
